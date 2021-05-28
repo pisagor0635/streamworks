e90640cd22211d3f2bddf43658eb96a5ded12bc7
ab268517@adalet.gov.tr <ab268517@adalet.gov.tr>
2021-05-28 16:09:26 +0300

    StreamWorks firstCommit

---
+++ src/StreamWorks.java
@@ -0,0 +1,61 @@
+public class StreamWorks {
+    public static void main(String[] args) {
+
+//        List<String> myList = Arrays.asList("a1", "b1", "a2", "c2", "c1");
+//        myList
+//                .stream()
+//                .filter(s -> s.startsWith("c"))
+//                .map(String::toUpperCase)
+//                .sorted()
+//                .forEach(System.out::println);
+
+
+//        Arrays.asList("a1", "a2", "a3")
+//                .stream()
+//                .findFirst()
+//                .ifPresent(System.out::println);
+
+//        Stream.of("a1","a2","a3")
+//                .findFirst()
+//                .ifPresent(System.out::println);
+
+//        IntStream.range(1,4)
+//                .forEach(System.out::println);
+
+//        Arrays.stream(new int[]{1, 2, 3})
+//                .map(n -> 2 * n + 1)
+//                .average()
+//                .ifPresent(System.out::println);
+
+//        Stream.of("a1", "a2", "a3")
+//                .map(s -> s.substring(1))
+//                .mapToInt(Integer::parseInt)
+//                .max()
+//                .ifPresent(System.out::println);
+
+//        IntStream.range(1, 4)
+//                .mapToObj(i -> "a" + i)
+//                .forEach(System.out::println);
+
+//        Stream.of(1.0, 2.0, 3.0)
+//                .mapToInt(Double::intValue)
+//                .mapToObj(i->"a" + i)
+//                .forEach(System.out::println);
+
+
+        //intermediate operations is laziness. Intermediate operations will only be executed when a terminal operation is present.
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .filter(s -> {
+//                    System.out.println("filter: " + s);
+//                    return true;
+//                });
+
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .filter(s -> {
+//                    System.out.println("filter: " + s);
+//                    return true;
+//                }).forEach(s -> System.out.println("forEach: " + s));
+
+
+    }
+}
