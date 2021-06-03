ef1bda39f807fb5034d263f4a526170e05359f06
ab268517@adalet.gov.tr <ab268517@adalet.gov.tr>
2021-06-03 10:53:19 +0300

    StreamWorks 2nd commit

--- src/StreamWorks.java
+++ src/StreamWorks.java
@@ -56,6 +56,80 @@ public class StreamWorks {
 //                    return true;
 //                }).forEach(s -> System.out.println("forEach: " + s));
 
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .map(s -> {
+//                    System.out.println("map: " + s);
+//                    return s.toUpperCase();
+//                })
+//                .anyMatch(s -> {
+//                    System.out.println("anyMatch: " + s);
+//                    return s.startsWith("A");
+//                });
+
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .map(s -> {
+//                    System.out.println("map: " + s);
+//                    return s.toUpperCase();
+//                })
+//                .filter(s -> {
+//                    System.out.println("filter: " + s);
+//                    return s.startsWith("A");
+//                })
+//                .forEach(s -> System.out.println("forEach: " + s));
+
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .filter(s -> {
+//                    System.out.println("filter: " + s);
+//                    return s.startsWith("a");
+//                })
+//                .map(s -> {
+//                    System.out.println("map: " + s);
+//                    return s.toUpperCase();
+//                })
+//                .forEach(s -> System.out.println("forEach: " + s));
+
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .sorted((s1, s2) -> {
+//                    System.out.printf("sort: %s; %s\n", s1, s2);
+//                    return s1.compareTo(s2);
+//                })
+//                .filter(s -> {
+//                    System.out.println("filter: " + s);
+//                    return s.startsWith("a");
+//                })
+//                .map(s -> {
+//                    System.out.println("map: " + s);
+//                    return s.toUpperCase();
+//                })
+//                .forEach(s -> System.out.println("forEach: " + s));
+
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .filter(s -> {
+//                    System.out.println("filter: " + s);
+//                    return s.startsWith("a");
+//                })
+//                .sorted((s1, s2) -> {
+//                    System.out.printf("sort: %s; %s\n", s1, s2);
+//                    return s1.compareTo(s2);
+//                })
+//                .map(s -> {
+//                    System.out.println("map: " + s);
+//                    return s.toUpperCase();
+//                })
+//                .forEach(s -> System.out.println("forEach: " + s));
+
+
+//        Stream<String> stream = Stream.of("d2", "a2", "b1", "b3", "c")
+//                .filter(s -> s.startsWith("a"));
+//        System.out.println(stream.anyMatch(s -> s.startsWith("a"))); //true
+//        System.out.println(stream.noneMatch(s -> s.startsWith("x"))); // exception (stream has been already operated)
+
+        //Stream supplier
+//        Supplier<Stream<String>> streamSupplier = () -> Stream.of("d2", "a2", "b1", "b3", "c")
+//                .filter(s -> s.startsWith("a"));
+//        streamSupplier.get().forEach(System.out::println);
+//        System.out.println(streamSupplier.get().anyMatch(s -> s.contains("a")));
+
 
     }
 }
